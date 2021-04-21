f0cba01dcc02f107422b0c0a13a25651567942e5
BohdanZyshchyk <bohdan.zyshchuk@zagravagames.com>
2021-04-21 14:41:51 +0300

    added sling

--- secondProjDemolition/Assets/Scripts/SlingShot.cs
+++ secondProjDemolition/Assets/Scripts/SlingShot.cs
@@ -2,17 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Slingshot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+
+    [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    void Awake()
     {
-        
+        Transform launchPointsTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointsTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+    void OnMouseEnter()
+    {
+        launchPoint.SetActive(true);
+        //print("Slingsot:OnMouseEnter");
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseExit()
     {
-        
+        launchPoint.SetActive(false);
+        //print("Slingsot:OnMouseExit");
+    }
+
+    void OnMouseDown()
+    {
+        // Игрок нажал кнопку мыши, когда указатель находился над рогаткой
+        aimingMode = true;
+        // Создать снаряд
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        // Поместить в точку launchPoint
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
