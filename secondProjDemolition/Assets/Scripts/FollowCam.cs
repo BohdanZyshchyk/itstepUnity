92769395bc5a5e6aebb08043565863b270ab0dfa
BohdanZyshchyk <bohdan.zyshchuk@zagravagames.com>
2021-04-25 12:27:47 +0300

    new beh

---
+++ secondProjDemolition/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI;
+    [Header("Set Dynamically")]
+    public float camZ;
+    
+    void Awake()
+    {
+        camZ = this.transform.position.z;    
+    }
+    void FixedUpdate()
+    {
+        if (POI == null) return; // выйти, если нет интересующего объекта
+        
+        // Получить позицию интересующего объекта
+        Vector3 destination = POI.transform.position;
+        
+        // Принудительно установить значение destination.z равным camZ, чтобы
+        // отодвинуть камеру подальше
+        destination.z = camZ;
+
+        // Поместить камеру в позицию destination
+
+        transform.position = destination;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- secondProjDemolition/Assets/Scripts/SlingShot.cs
+++ secondProjDemolition/Assets/Scripts/SlingShot.cs
@@ -7,7 +7,7 @@ public class Slingshot : MonoBehaviour
 
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
-    public float velocityMult = 8f;
+    public float velocityMult = 20f;
 
     [Header("Set Dynamically")]
     public GameObject launchPoint;
