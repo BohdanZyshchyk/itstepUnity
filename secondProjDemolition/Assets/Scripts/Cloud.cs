34761198632e4dd3a1487266a3b8e554f4044413
BohdanZyshchyk <bohdan.zyshchuk@zagravagames.com>
2021-04-26 23:41:40 +0300

    clouds

---
+++ secondProjDemolition/Assets/Scripts/Cloud.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject cloudSphere;
+    public int numSpheresMin = 6;
+    public int numSpheresMax = 6;
+    public Vector3 sphereOffsetScale = new Vector3(5, 2, 1);
+    public Vector2 sphereScaleRangeX = new Vector2(4, 8);
+    public Vector2 sphereScaleRangeY = new Vector2(3, 4);
+    public Vector2 sphereScaleRangeZ = new Vector2(2, 4);
+    public float scaleYMin = 2f;
+
+    private List<GameObject> spheres;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spheres = new List<GameObject>();
+        int num = Random.Range(numSpheresMin, numSpheresMax);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- secondProjDemolition/Assets/Scripts/FollowCam.cs
+++ secondProjDemolition/Assets/Scripts/FollowCam.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class FollowCam : MonoBehaviour
 {
     static public GameObject POI;
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
     [Header("Set Dynamically")]
     public float camZ;
     
@@ -18,14 +21,21 @@ public class FollowCam : MonoBehaviour
         
         // Получить позицию интересующего объекта
         Vector3 destination = POI.transform.position;
-        
+        // Ограничить X и Y минимальными значениями
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        // Определить точку между текущим местоположением камеры и destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
         // Принудительно установить значение destination.z равным camZ, чтобы
         // отодвинуть камеру подальше
         destination.z = camZ;
 
         // Поместить камеру в позицию destination
-
         transform.position = destination;
+        // Изменить размер orthographicSize камеры., чтобы земля
+        // оставалась в поле зрения
+        Camera.main.orthographicSize = destination.y + 10;
+
     }
     // Start is called before the first frame update
     void Start()
